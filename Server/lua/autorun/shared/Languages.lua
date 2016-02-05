647eb89e2ef7da93f65430e1c4015f61625858be
gtnardy <gtnardy@gmail.com>
2016-02-05 16:39:29 -0200

    Languages

---
+++ Server/lua/autorun/shared/Languages.lua
@@ -0,0 +1,31 @@
+class 'Languages'
+
+function Languages:__init()
+	
+	self.language = "pt"
+	self.alias = {}
+	
+	Events:Subscribe("NetworkObjectValueChange", self, self.ObjectValueChange)
+	Events:Subscribe("SharedObjectValueChange", self, self.ObjectValueChange)
+end
+
+
+function Languages:ObjectValueChange(args)
+	if args.object.__type == "Player" and args.object == LocalPlayer and args.key == "Language" then
+		self.language = LocalPlayer:GetValue("Language")
+		self:UpdateValues()
+	end
+end
+
+
+function Languages:SetLanguage(alias, languages)
+	self.alias.alias = languages
+	self[alias] = languages[self.language]
+end
+
+
+function Languages:UpdateValues()
+	for key, words in pairs(self.alias) do
+		self[key] = words[self.language]
+	end
+end
