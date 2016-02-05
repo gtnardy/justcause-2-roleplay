d96336c7880025eb46a353b949d4d7bd3fb8d223
gtnardy <gtnardy@gmail.com>
2016-02-05 16:38:46 -0200

    Controls

--- Server/scripts/Controls/client/Controls.lua
+++ Server/scripts/Controls/client/Controls.lua
@@ -7,27 +7,42 @@ function Controls:__init()
 	Events:Subscribe("GameLoad", self, self.GameLoad)
 	Events:Subscribe("CalcView", self, self.CalcView)
 	Events:Subscribe("Render", self, self.Render)
+	Events:Subscribe("KeyUp", self, self.KeyUp)
 
+	self.cameraAtual = 1
 	self.mirar = false
 	self.imageReticle = Image.Create(AssetLocation.Resource, "Sniper_Reticle")
 	
 end
 
+
 function Controls:GameLoad(args)
-	Game:FireEvent("ply.grappling.enable")
+	Game:FireEvent("ply.grappling.disable")
 	Game:FireEvent("ply.parachute.disable")
 end
 
 
 function Controls:MouseDown(args)
-	if (self:GetGameState() and args.button == 2 and not LocalPlayer:InVehicle()) then
+	if (Game:GetGUIState() == GUIState.Game and args.button == 2 and not LocalPlayer:InVehicle()) then
 		self.mirar = true
 	end
 end
 
 
+function Controls:KeyUp(args)
+	if (Game:GetGUIState() == GUIState.Game and args.key == string.byte("V")) then
+		self:TrocarCamera()
+	end
+end
+
+
+function Controls:TrocarCamera()
+	self.cameraAtual = (self.cameraAtual + 1) % 3 + 1
+end
+
+
 function Controls:InputPoll(args)
-	if self:GetGameState() then
+	if Game:GetGUIState() == GUIState.Game then
 		if self.mirar then
 			Input:SetValue(Action.ShoulderCam, 1)
 			self.mirar = false
@@ -40,9 +55,29 @@ end
 
 
 function Controls:CalcView()
+	-- local angle = Camera:GetAngle()
+	-- local playerPosition = LocalPlayer:GetPosition() + Vector3(0, 1, 0)
+	
 	if Camera:GetFOV() >= 0.69 then
 		Camera:SetFOV(0.9)
 	end
+	
+	-- if Camera:GetFOV() > 0.89 then
+		-- Chat:Print("ya", Color(10, 10, 200))
+		-- local zoom = 0
+
+		-- if self.cameraAtual == 1 then
+			-- zoom = -1
+		-- elseif self.cameraAtual == 2 then
+			-- zoom = 2
+		-- elseif self.cameraAtual == 3 then
+			-- zoom = 3
+		-- end
+		
+		-- local cam_pos = Camera:GetPosition()
+		-- cam_pos = cam_pos + ( Camera:GetAngle() * Vector3( 0, 0, zoom ) )
+		-- Camera:SetPosition(cam_pos)		
+	-- end
 end
 
 
@@ -62,14 +97,6 @@ function Controls:Render()
 			Render:FillCircle(Render.Size / 2, 2, Color(255, 255, 255, 125))
 		end		
 	end
-end
-
-
-function Controls:GetGameState()
-	if(Game:GetState() != GUIState.PDA and Game:GetState() != GUIState.Loading and Game:GetState() != GUIState.Menu) then
-		return true
-	end
-	return false
 end
 
 
