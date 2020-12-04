124bae5bb92dee5fd71f42174a074001cc9f135c
Gabriel T. Nardy <gtnardy@gmail.com>
2020-12-04 19:19:58 -0300

    Deleted useless site & forum files

--- Arquivos/FTP/forum/cache/data_versioncheck_boardtools.github.io-uploadupload.json.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1452355103
-546
-a:2:{s:6:"stable";a:1:{s:3:"3.1";a:5:{s:7:"current";s:9:"3.1.2-RC3";s:12:"announcement";s:66:"https://www.phpbb.com/community/viewtopic.php?p=14296546#p14296546";s:8:"download";s:56:"https://github.com/BoardTools/upload/archive/develop.zip";s:3:"eol";N;s:8:"security";b:0;}}s:8:"unstable";a:1:{s:3:"3.1";a:5:{s:7:"current";s:9:"3.1.2-RC3";s:12:"announcement";s:66:"https://www.phpbb.com/community/viewtopic.php?p=14296546#p14296546";s:8:"download";s:56:"https://github.com/BoardTools/upload/archive/develop.zip";s:3:"eol";N;s:8:"security";b:0;}}}
--- Arquivos/FTP/forum/ext/boardtools/upload/language/README.md
+++
@@ -1,3 +0,0 @@
-## Language packages
-Available language packages and the information about translations can be found here:
-https://github.com/BoardTools/upload/wiki/Translations
